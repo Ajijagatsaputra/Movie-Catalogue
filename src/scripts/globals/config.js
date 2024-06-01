8bf9fdc703e563806f17d902b36b69a85dbf80d2
Ajijagatsaputra <ajijagat90@gmail.com>
2024-06-02 00:25:39 +0700

    moviecatalogue

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,9 @@
+const CONFIG = {
+  KEY: '5ee8fc5d6d0df47151df9932c6191117',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+  CACHE_NAME: 'MovieCatalogue-V1',
+};
+
+export default CONFIG;
